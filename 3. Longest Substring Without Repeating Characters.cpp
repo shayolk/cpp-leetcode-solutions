739f1d30c9b46c4d6e7d119f7d6e8aa2f592e4f4
Shayak <44469461+shayolk@users.noreply.github.com>
2026-05-30 14:10:26 +0100

    https://leetcode.com/problems/longest-substring-without-repeating-characters/
    https://leetcode.com/problems/longest-substring-without-repeating-characters/

--- 3. Longest Substring Without Repeating Characters.cpp
+++ 3. Longest Substring Without Repeating Characters.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         vector<int> last(128, -1);
-        int n = s.size(), ans = 0, cnt = 0;
+        int l = 0, r = 0, n = s.size(), ans = 0;
         for (int i = 0; i < n; ++i) {
-            if (last[s[i]] == -1 || last[s[i]] < i - cnt) {
-                ++cnt;
-            } else {
-                cnt = i - last[s[i]];
+            r = i;
+            if (last[s[i]] != -1) {
+                l = max(l, last[s[i]] + 1);
             }
             last[s[i]] = i;
-            ans = max(ans, cnt);
+            ans = max(ans, r - l + 1);
         }
         return ans;
     }
