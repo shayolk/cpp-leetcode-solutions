59c98ef2bd0b40fd9e602af9bf0bd40eacce1784
Shayak <44469461+shayolk@users.noreply.github.com>
2026-06-01 14:56:04 +0100

    https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/
    https://leetcode.com/problems/final-prices-with-a-special-discount-in-a-shop/

---
+++ 1475. Final Prices With a Special Discount in a Shop.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        int n = prices.size();
+        stack<int> st;
+        vector<int> ans(n);
+        for (int i = n - 1; i >= 0; --i) {
+            while (!st.empty() && st.top() > prices[i]) {
+                st.pop();
+            }
+            ans[i] = prices[i];
+            if (!st.empty()) {
+                ans[i] -= st.top();
+            }
+            st.push(prices[i]);
+        }
+        return ans;
+    }
+};
