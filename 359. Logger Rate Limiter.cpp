af1ef155fda675ac8b13feb214807ff43e890b29
Shayak <44469461+shayolk@users.noreply.github.com>
2026-07-20 20:39:40 +0100

    https://leetcode.com/problems/logger-rate-limiter/
    https://leetcode.com/problems/logger-rate-limiter/

---
+++ 359. Logger Rate Limiter.cpp
@@ -0,0 +1,20 @@
+class Logger {
+    map<string,int> times;
+
+public:
+    Logger() {}
+    
+    bool shouldPrintMessage(int timestamp, string message) {
+        if (times.count(message) && timestamp < times[message]) {
+            return false;
+        }
+        times[message] = timestamp + 10;
+        return true;
+    }
+};
+
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger* obj = new Logger();
+ * bool param_1 = obj->shouldPrintMessage(timestamp,message);
+ */
